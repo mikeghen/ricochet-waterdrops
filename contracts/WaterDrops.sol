df0c3d1d2fc67deea5a07366417c14d9e1e6358b
Michael Ghen <mike@mikeghen.com>
2022-09-18 23:24:28 -0400

    Superfluid cfa integration

--- contracts/WaterDrops.sol
+++ contracts/WaterDrops.sol
@@ -1,10 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import {
+    ISuperfluid
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol"; //"@superfluid-finance/ethereum-monorepo/packages/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+
+import {
+    IConstantFlowAgreementV1
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+
+import {
+    CFAv1Library
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
+
 // Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract WaterDrops {
+
+  using CFAv1Library for CFAv1Library.InitData;
 
   struct Claim {
     ISuperToken token
@@ -19,7 +33,15 @@ contract WaterDrops {
   address[] closureQueue;
   uint queueIndex;
   address owner;
+  CFAv1Library.InitData public cfaV1;
+  IConstantFlowAgreementV1 internal cfa; // The stored constant flow agreement class address
 
+
+  constructor(ISuperfluid host, IConstantFlowAgreementV1 _cfa) {
+
+    cfa = _cfa;
+    cfaV1 = CFAv1Library.InitData(host, IConstantFlowAgreementV1(address(host.getAgreementClass(keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1")))));
+  }
 
   addClaim(ISuperToken token, uint rate, uint duration, uint deadline) public onlyOwner {
 
@@ -40,7 +62,7 @@ contract WaterDrops {
 
     require(userClaims[msg.sender] != 0, 'no claims');
     closureQueue.push(msg.sender);
-    startStreaming(msg.sender, token, rate);
+    cfaV1.createFlow(msg.sender, claims[userClaims[msg.sender]].token, fclaims[userClaims[msg.sender]].rate);
 
   }
 
@@ -48,9 +70,29 @@ contract WaterDrops {
 
     address toClose = closureQueue[queueIndex];
 
+    ( uint256 timestamp,
+      int96 flowRate,
+      uint256 deposit,
+      uint256 owedDeposit) = cfa.getFlow(
+        claims[userClaims[toClose]].token,
+        toClose,
+        address(this)
+    );
+
     // Two ways to check:
     // 1. When did the stream start? Has duration amount of time passed?
-    // 2. How much has been streamed to this receipient so far? Is it more than rate * duration?
+
+    // How much time has passed?
+    timestamp = block.timestamp - timestamp;
+
+    // Is that larger than the duration?
+    if (timestamp > duration) {
+      // Streams over, close stram to toClose
+      cfaV1.deleteFlow(toClose, claims[userClaims[toClose]].token);
+    } else {
+      // If we don't need to close, revert with message for Gelato
+      revert('not ready to close');
+    }
 
   }
 
